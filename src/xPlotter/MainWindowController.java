08c3ee2a27e1ed8cc068e8a25d2cf850455a632f
Alex <twistx77@gmail.com>
2018-07-04 17:17:19 +0200

    Added XChart

--- src/xPlotter/MainWindowController.java
+++ src/xPlotter/MainWindowController.java
@@ -1,14 +1,19 @@
 package xPlotter;
 
 import javafx.application.Platform;
+import javafx.embed.swing.SwingNode;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
-import javafx.scene.chart.XYChart;
+//import javafx.scene.chart.XYChart;
 import javafx.scene.control.ComboBox;
+import org.knowm.xchart.QuickChart;
+import org.knowm.xchart.SwingWrapper;
+import org.knowm.xchart.XChartPanel;
+import org.knowm.xchart.XYChart;
 
+import javax.swing.*;
 import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -26,13 +31,19 @@ public class MainWindowController implements Initializable  {
     private LineChart<Number, Number> plot;
 
     @FXML
-    NumberAxis xAxis = new NumberAxis();
+    NumberAxis xAxis;
 
     @FXML
-    NumberAxis yAxis = new NumberAxis();
+    NumberAxis yAxis;
 
+    @FXML
+    SwingNode node;
 
     ComProtocol comProtocol;
+
+    double phase = 0;
+    double[][] initdata = getSineData(phase);
+
 
 
 
@@ -56,7 +67,26 @@ public class MainWindowController implements Initializable  {
 
     public void startChart(){
 
+        double[] xData = new double[] { 0.0, 1.0, 2.0 };
+        double[] yData = new double[] { 2.0, 1.0, 0.0 };
 
+
+
+        final XYChart chart = QuickChart.getChart("Simple XChart Real-time Demo", "Radians", "Sine", "sine", initdata[0], initdata[1]);
+
+
+        // Show it
+        final SwingWrapper<XYChart> sw = new SwingWrapper<XYChart>(chart);
+        sw.displayChart();
+
+        JPanel pnlChart = new XChartPanel(chart);
+        // panel.setLayout(null);
+        //panel.add(new SwingWrapper(chart));
+
+// Show it
+
+
+        node.setContent(pnlChart);
 
         xAxis.setAnimated(false);
         xAxis.setLabel("Tick");
@@ -64,23 +94,29 @@ public class MainWindowController implements Initializable  {
         yAxis.setAnimated(false);
         yAxis.setLabel("Value");
 
-        XYChart.Series<Number, Number> series = new XYChart.Series<>();
-        series.setName("Values");
+       // XYChart.Series<Number, Number> series = new XYChart.Series<>();
+       // series.setName("Values");
 
         //plot = new LineChart<>(xAxis, yAxis);
-        plot = new LineChart<>(xAxis, yAxis);
+        //plot = new LineChart<>(xAxis, yAxis);
         plot.setAnimated(false);
-        plot.getData().add(series);
+      //  plot.getData().add(series);
 
         Thread updateThread = new Thread(() -> {
             while (true) {
                 try {
-                    Thread.sleep(1000);
+
+                    phase += 2 * Math.PI * 2 / 20.0;
+
+                    Thread.sleep(100);
+
+                    final double[][] data = getSineData(phase);
+
                     //series.getData().add(new XYChart.Data<>(tick.incrementAndGet(), (int) (Math.random() * 100)));
-                    Platform.runLater(() -> series.getData().add(new XYChart.Data<>(tick.incrementAndGet(), (int) (Math.random() * 100))));
-
-
-                    System.out.println(tick.incrementAndGet() + " " + (int) (Math.random() * 100));
+                    //Platform.runLater(() -> series.getData().add(new XYChart.Data<>(tick.incrementAndGet(), (int) (Math.random() * 100))));
+                    Platform.runLater(() -> chart.updateXYSeries("sine", data[0], data[1], null));
+                    sw.repaintChart();
+                    System.out.println(tick.incrementAndGet() + " " + data[0]);
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
@@ -95,5 +131,17 @@ public class MainWindowController implements Initializable  {
 
 
 
+    }
+
+    private static double[][] getSineData(double phase) {
+
+        double[] xData = new double[1000];
+        double[] yData = new double[1000];
+        for (int i = 0; i < xData.length; i++) {
+            double radians = phase + (2 * Math.PI / xData.length * i);
+            xData[i] = radians;
+            yData[i] = Math.sin(radians);
+        }
+        return new double[][] { xData, yData };
     }
 }
